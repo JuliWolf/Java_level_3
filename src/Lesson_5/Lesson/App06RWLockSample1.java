92167bdd45f9c41c827c92efc0ee46fd4d7abaf7
Julia Ivanova <for.julia96@gmail.com>
2022-05-14 16:14:18 +0300

    Add lesson 5

---
+++ src/Lesson_5/Lesson/App06RWLockSample1.java
@@ -0,0 +1,51 @@
+package Lesson_5.Lesson;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class App06RWLockSample1 {
+    // Интерфейс java.util.concurrent.locks.ReadWriteLock – это продвинутый
+    // механизм для блокировки потоков. Он позволяет множеству потоков
+    // одновременно читать данные, или только одному потоку – их записывать.
+    // Ресурс открыт для чтения множеству потоков без риска ошибок.
+
+    // Основные правила:
+    // Read Lock − если ни один поток не удерживает лок для записи, то любое
+    // количество потоков могут взять лок для чтения
+    // Write Lock − если ни один поток не удерживает лок для чтения или для
+    // записи, то один поток может взять лок на запись
+
+    public static void main(String[] args) {
+        final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        for (int i = 0; i < 3; i++) {
+            int index = i;
+            new Thread(() -> {
+                readWriteLock.readLock().lock();
+                try {
+                    System.out.println("Начало чтения - " + index);
+                    Thread.sleep(1000);
+                    System.out.println("Завершение чтения - " + index);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    readWriteLock.readLock().unlock();
+                }
+            }).start();
+        }
+        for (int i = 0; i < 2; i++) {
+            int index = i;
+            new Thread(() -> {
+                readWriteLock.writeLock().lock();
+                try {
+                    System.out.println("Начало записи - " + index);
+                    Thread.sleep(1000);
+                    System.out.println("Завершение записи - " + index);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    readWriteLock.writeLock().unlock();
+                }
+            }).start();
+        }
+    }
+}
---
+++ src/Lesson_5/Lesson/App07Semaphore.java
@@ -0,0 +1,36 @@
+package Lesson_5.Lesson;
+
+import java.util.concurrent.Semaphore;
+
+public class App07Semaphore {
+    public static void main(String[] args) {
+        final Semaphore smp = new Semaphore(4);
+        Runnable limitedCall = new Runnable() {
+            int count = 0;
+            public void run() {
+                int time = 3 + (int)(Math.random() * 4.0);
+                int num = count++;
+                try {
+                    // в этой секции поток захватывает семафор,
+                    // если в нем есть свободные места, если мест нет, ждет
+                    // пока не появится место
+                    smp.acquire();
+                    System.out.println("Поток #" + num + " начинает выполнять очень долгое действие " + time + " сек.");
+                    // делаем вид, что поток выполняет важную задачу
+                    Thread.sleep(time * 1000);
+                    System.out.println("Поток #" + num + " завершил работу!");
+                } catch (InterruptedException intEx) {
+                    intEx.printStackTrace();
+                } finally {
+                    // освобождаем семафор, чтобы другой поток мог его занять
+                    smp.release();
+                }
+            }
+        };
+        for (int i = 0; i < 10; i++)
+            // пытаемся запустить одновременно 10 потоков
+            new Thread(limitedCall).start();
+    }
+
+
+}
